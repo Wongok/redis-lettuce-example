a1a94d076a75380bb4fce7363db88663ed539475
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2019-12-09 20:26:06 +0900

    create redis-lettuce project

---
+++ src/main/java/boot/test/redis/RedisLettuceExampleApplication.java
@@ -0,0 +1,13 @@
+package boot.test.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLettuceExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLettuceExampleApplication.class, args);
+    }
+
+}
